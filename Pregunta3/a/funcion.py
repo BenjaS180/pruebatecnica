86b588cb5ff03db1aa01664ba9df138d0790a0a8
BSbenjaminsoto <benjaminsoto661@hotmail.com>
2023-12-23 12:44:22 -0300

    nuevamente cambios a funcion de python

--- Pregunta3/a/funcion.py
+++ Pregunta3/a/funcion.py
@@ -1,25 +1,25 @@
-def agrupar_productos(datos_query):
-    productos_agrupados = {}
+def group_products(query_data):
+    grouped_products = {}
     
-    for producto in datos_query:
-        ean = producto['Ean']
-        nombre_producto = producto['nombre_producto']
-        datos_query_producto = producto['datos_query']
-        cantidad_markets = producto['cantidad_markets']
-        precios = [float(precio) for precio in producto['Precios']]
-        rango_precios = max(precios) - min(precios)
+    for product in query_data:
+        ean = product['Ean']
+        product_name = product['nombre_producto']
+        query_data_product = product['datos_query']
+        market_count = product['cantidad_markets']
+        prices = [float(price) for price in product['Precios']]
+        price_range = max(prices) - min(prices)
         
-        if ean not in productos_agrupados:
-            productos_agrupados[ean] = {
-                'nombre_producto': nombre_producto,
-                'datos_query': [datos_query_producto],
-                'cantidad_markets': 1,
-                'rango_precios': rango_precios
+        if ean not in grouped_products:
+            grouped_products[ean] = {
+                'product_name': product_name,
+                'query_data': [query_data_product],
+                'market_count': 1,
+                'price_range': price_range
             }
         else:
-            productos_agrupados[ean]['datos_query'].append(datos_query_producto)
-            productos_agrupados[ean]['cantidad_markets'] += 1
-            productos_agrupados[ean]['rango_precios'] = max(productos_agrupados[ean]['rango_precios'], rango_precios)
+            grouped_products[ean]['query_data'].append(query_data_product)
+            grouped_products[ean]['market_count'] += 1
+            grouped_products[ean]['price_range'] = max(grouped_products[ean]['price_range'], price_range)
     
-    resultado_final = [{'Ean': ean, **info} for ean, info in productos_agrupados.items()]
-    return resultado_final
+    final_result = [{'Ean': ean, **info} for ean, info in grouped_products.items()]
+    return final_result
